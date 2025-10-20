8e0b0dddc2958cc862bd7a0de91c81859d962ad5
Maykmora <maykmora12@gmail.com>
2025-10-19 23:52:49 -0600

    Prueba del tkinter

---
+++ Construccion_interfaz.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, simpledialog
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tiendita")
+        self.geometry("900x600")
+        self.resizable(True, True)
+
+        tab_control=ttk.Notebook(self)
+        self.tab_productos= ttk.Frame(tab_control)
+        self.tab_ventas= ttk.Frame(tab_control)
+        self.tab_reportes= ttk.Frame(tab_control)
+        tab_control.add(self.tab_productos, text="Agregar productos")
+        tab_control.add(self.tab_ventas, text='Ventas')
+        tab_control.add(self.tab_reportes, text='Reportes')
+        tab_control.pack(expand=1, fill='both')
+
+        self._build_productos_tab()
+    def _build_productos_tab(self):
+        frame_left= ttk.Frame(self.tab_productos, padding=10)
+        frame_left.pack(side="left", fill="y")
+
+        ttk.Label(frame_left, text="Código:").pack(anchor='w')
+        self.entry_codigo = ttk.Entry(frame_left)
+        self.entry_codigo.pack(fill='x')
+
+        ttk.Label(frame_left, text="Nombre:").pack(anchor='w')
+        self.entry_nombre = ttk.Entry(frame_left)
+        self.entry_nombre.pack(fill='x')
+
+        ttk.Label(frame_left, text="Existencias:").pack(anchor='w')
+        self.entry_existencia = ttk.Entry(frame_left)
+        self.entry_existencia.pack(fill='x')
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
