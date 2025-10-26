6840f7c234832af470b0aac92b8c924f58781022
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-10-26 13:00:34 -0600

    Se eliminó el metodo registrar producto de la clase ProductoDB porque el metodo guardar hace lo mismo.

---
+++ modificacion_sql.py
@@ -0,0 +1,118 @@
+import sqlite3
+
+DB_NAME = "productos.db"
+
+class Productos:
+    def __init__(self, codigo, nombre, precio, categoria, cantidad):
+        self.__codigo = codigo
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__categoria = categoria
+        self.__cantidad = cantidad
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, new_nombre):
+        if new_nombre:
+            self.__nombre = new_nombre
+        else:
+            print("El campo no puede estar vacio")
+
+    @property
+    def precio(self):
+        return self.__precio
+
+    @precio.setter
+    def precio(self, new_precio):
+        if new_precio:
+            self.__precio = new_precio
+        else:
+            print("El campo no puede estar vacio")
+
+    @property
+    def categoria(self):
+        return self.__categoria
+
+    @property
+    def cantidad(self):
+        return self.__cantidad
+
+    @cantidad.setter
+    def cantidad(self, new_cantidad):
+        if new_cantidad:
+            self.__cantidad = new_cantidad
+        else:
+            print("El campo no puede estar vacio")
+
+class ProductosDB:
+    DB_NAME = "productos.db"
+
+    @staticmethod
+    def _conn():
+        conn = sqlite3.connect(ProductosDB.DB_NAME)
+        conn.row_factory = sqlite3.Row
+        conn.execute("""
+                CREATE TABLE IF NOT EXISTS productos (
+                    id_num INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre TEXT NOT NULL,
+                    codigo TEXT NOT NULL,
+                    precio REAL,
+                    categoria TEXT NOT NULL,
+                    cantidad REAL
+                );
+            """)
+        conn.commit()
+        return conn
+
+    @staticmethod
+    def guardar(producto: Productos):
+        with ProductosDB._conn() as conn:
+            conn.execute(
+                "INSERT INTO productos (nombre, codigo, precio, categoria, cantidad) VALUES (?, ?, ?, ?, ?)",
+                (producto.nombre, producto.codigo, producto.precio, producto.categoria, producto.cantidad)
+            )
+        print(f"Productos '{producto.nombre}' guardado con éxito.")
+
+
+
+    @staticmethod
+    def eliminar_producto():
+        codigo = input("Ingrese el codigo del producto a eliminar eliminar: ")
+        with ProductosDB._conn() as conn:
+            cur = conn.execute("DELETE FROM productos WHERE codigo = ?", (codigo,))
+            if cur.rowcount == 0:
+                print("No se encontró el producto.")
+            else:
+                print("Producto eliminado con éxito.")
+
+    @staticmethod
+    def consultar_producto():
+        consulta = input("Ingrese la categoria a buscar: ")
+
+
+while True:
+    print("---Menú----")
+    print("1.- Registrar producto")
+    print("2.- Ver productos")
+    print("3.- Eliminar producto")
+    menu_option = input("Ingrese el número de la opción que quiera realizar: ")
+    print()
+    match menu_option:
+        case "1":
+            print("Registrar producto")
+            ProductosDB.registrar_producto()
+            print()
+        case "2":
+            print("Ver productos")
+            ProductosDB.ver_productos()
+            print()
+        case "3":
+            print("Eliminar producto")
+            ProductosDB.elim
